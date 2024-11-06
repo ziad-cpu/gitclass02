93f3f5d4f41c6aeb50c86a52d56b16aacecff47a
ziad-cpu <hziademp@gmail.com>
2024-11-06 18:25:22 -0500

    fourthcommit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,6 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("This is the project version 3");
+        System.out.println("This is the project version 4");
     }
 }
-//done with version 3
+//done with version 4
