fe55f0ba9f45946ed3927f17e711f9f4550d8f3e
ziad-cpu <hziademp@gmail.com>
2024-11-05 23:20:51 -0500

    firstcommit

---
+++ src/test/java/test.java
@@ -0,0 +1,7 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("This is the project version 1");
+    }
+}
+//done with version 1
