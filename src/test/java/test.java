46df6c1f68890af4a9c0debe30c098b665354725
ziad-cpu <hziademp@gmail.com>
2024-11-05 23:39:39 -0500

    secondcommit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,7 +1,12 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is the project version 1");
+        System.out.println("This is the project version 2");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+
     }
 }
-//done with version 1
+//done with version 2
