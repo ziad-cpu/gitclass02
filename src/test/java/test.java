b609ff38e55bb619e257d32d613ab4418f67ea8c
ziad-cpu <hziademp@gmail.com>
2024-11-05 23:42:43 -0500

    thirdcommit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,12 +1,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("This is the project version 2");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-        System.out.println("Hello World");
-
+        System.out.println("This is the project version 3");
     }
 }
-//done with version 2
+//done with version 3
